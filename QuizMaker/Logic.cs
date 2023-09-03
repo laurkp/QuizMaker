9269f96095b4333e2f6c9223747806890b77a734
laurkp <laurentziukp@gmail.com>
2023-09-03 16:07:56 +0100

    Add project files.

---
+++ QuizMaker/Logic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace QuizMaker
+{
+    internal class Logic
+    {
+        /// <summary>
+        /// Defining a serializable class called 'Question'
+        /// </summary>
+        [Serializable]
+        public class Question
+        {
+            // Property to store the text of the question
+            public string QuestionText { get; set; }
+            // Property to store a list of possible answer choices
+            public List<string> Choices { get; set; }
+            // Property to store a list of indices representing correct choices
+            public List<int> CorrectChoices { get; set; }
+        }
+    }
+}
---
+++ QuizMaker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuizMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a continuous loop for the quiz program
+            while (true)
+            {
+                // Prompt the user to choose a mode using UIQuiz.Choice() method
+                string mode = UIQuiz.Choice();
+
+                // Using a switch statement to handle the chosen mode
+                switch (mode)
+                {
+                    case "1":
+                        UIQuiz.AddQuestion();
+                        break;
+                    case "2":
+                        UIQuiz.StartQuiz();
+                        break;
+                    case "3":
+                        string saveFileName = UIQuiz.SaveFile();
+                        UIQuiz.SaveQuestions(saveFileName);
+                        break;
+                    case "4":
+                        string loadFileName = UIQuiz.LoadFile();   
+                        UIQuiz.LoadQuestions(loadFileName);
+                        break;
+                    case "5":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        UIQuiz.InvalidChoice(); 
+                        break;
+                }
+            }
+        }
+    }
+}
